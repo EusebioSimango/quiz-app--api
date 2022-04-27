60131299fc35b4c5c6596a0442c9241ffbf496a2
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-04-28 01:22:06 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,11 +4,16 @@ const filename = join(__dirname, "../database", "db.json")
 
 const generateData = async () => {
 	const { find, create } = await QuestionsData(filename)
-	console.log(await (find()))
-	return [find, create]
+	// console.log(await (find()))
+	// const data = await find()
+	return { find, create }
 }
 
-// module.exports = { generateData }
+module.exports = { generateData }
 
-const [ find, create ] = generateData()
-console.log(find)
+async function func() {
+	const { find, create } = await generateData()
+	console.log(find, create)
+}
+
+func()
