2a7fb37ca0c299262883cfd26a4c239d4f787f49
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-12 19:11:32 +0200

    start using express

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const HTTP = require("http")
+const express = require('express')
+const cors = require('cors')
 const PORT = 3333
 const DEFAULT_HEADER = {
 	"Content-Type": "application/json"
@@ -23,22 +25,22 @@ const db = [{
 const routes = {
 	"/": async (request, response) => {
 		response.write('API MADE BY EUSEBIO SIMANGO')
-		response.end()		
+		response.end()
 	},
 
 	"/questions/all:get": async (request, response) => {
 		const data = db
-		response.write(JSON.stringify( data ))
+		response.write(JSON.stringify(data))
 		response.end()
 	},
 	"/questions/all:post": async (request, response) => {
 		for await (const data of request) {
 			try {
-				const question      = JSON.parse(data)
-				const isQuestion    = (!!question.question)
-				const isOptions     = (!!question.a && !!question.b && !!question.c && !!question.c)
+				const question = JSON.parse(data)
+				const isQuestion = (!!question.question)
+				const isOptions = (!!question.a && !!question.b && !!question.c && !!question.c)
 				const isRightOption = (!!question.rightAnswer)
-				
+
 				if (isQuestion && isOptions && isRightOption) {
 					db.push(question)
 					response.writeHead(201, DEFAULT_HEADER)
@@ -48,15 +50,15 @@ const routes = {
 
 					return response.end()
 				}
-				
+
 				response.writeHead(201, DEFAULT_HEADER)
 				response.write(JSON.stringify({
 					error: 'Missing anything!!'
 				}))
 
 				return response.end()
-				
-				
+
+
 			} catch (error) {
 				return handlerError(response)(error)
 			}
@@ -73,7 +75,7 @@ const routes = {
 const handlerError = response => {
 	return error => {
 		console.log('Deu Ruim', error)
-		response.writeHead(500,DEFAULT_HEADER)
+		response.writeHead(500, DEFAULT_HEADER)
 		response.write(JSON.stringify({
 			error: 'Internal Server Error!!'
 		}))
@@ -84,19 +86,19 @@ const handlerError = response => {
 const handler = (request, response) => {
 
 	const { url, method } = request
-	const [ home, questions, all ] = url.split('/')
+	const [home, questions, all] = url.split('/')
 	//Routes 
 	const api = `/${questions}/${all}:${method.toLowerCase()}`
 
 	const key = (questions && all) ? api : `/`
 
 	console.log(key)
-	const chosen = routes[key] ? routes[key] : routes["404"] 
+	const chosen = routes[key] ? routes[key] : routes["404"]
 
 	request.setHeader('Access-Control-Allow-Origin', '*')
 	request.setHeader('Access-Control-Request-Method', '*')
 	request.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-	request.setHeader('Access-Control-Allow-Headers', '*')	
+	request.setHeader('Access-Control-Allow-Headers', '*')
 	response.setHeader('Access-Control-Allow-Origin', '*')
 	response.setHeader('Access-Control-Request-Method', '*')
 	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
