eed98c9d0a3607b53e849dfc9ed50ad6024d9918
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-19 02:41:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(cors({
  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }))
  
+app.use((request, response, next) => {
+    response.append('Access-Control-Allow-Origin', ['*']);
+    response.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    response.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(routes)
 
 app.listen(PORT, () => console.log('Server runing at port:', PORT))
